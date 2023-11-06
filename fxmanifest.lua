0aec4b5024a36cc40a369db407a60b93c2e2dd1c
Renčikas <rencikas.business@gmail.com>
2023-11-06 17:31:42 +0200

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+    'starter_shells_k4mb1.ytyp',
+	'data/custom_sounds.dat54.rel',
+	'audiodirectory/custom_sound.awc',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
+data_file 'AUDIO_WAVEPACK' 'audiodirectory'
+data_file 'AUDIO_SOUNDDATA' 'data/custom_sounds.dat'
